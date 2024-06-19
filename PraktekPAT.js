5bd1eecec695ed5e0d6ae1007a5c267994ad7caa
Zainn <153479531+TheZainn@users.noreply.github.com>
2024-06-19 10:51:18 +0700

    Update PraktekPAT.js

--- PraktekPAT.js
+++ PraktekPAT.js
@@ -1,1 +1,40 @@
+// SOAL NO 1
+class Gadget {
+    constructor(brand, type){
+        this.brand = brand;
+        this.type = type;
+    }
+}
 
+//SOAL NO 2
+class Laptop extends Gadget {
+    constructor(brand, type) {
+        super(brand,type,true);
+    }
+
+    run() {
+        return (this.brand + " sedang berjalan");
+    }
+}
+
+//SOAL NO 3
+class Phone extends Gadget {
+    constructor(brand, type) {
+        super(brand, type, false);
+    }
+    run() {
+        return (this.brand + " sedang berjalan");
+    }
+}
+
+//SOAL NO 4
+const myLaptop = new Laptop("ASUS", "Zephyrus",);
+
+//SOAL NO 5
+const myPhone = new Phone("Samsung","S23");
+
+//Output
+console.log(myLaptop.run());
+console.log(myPhone.run());
+console.log(myLaptop);
+console.log(myPhone);
